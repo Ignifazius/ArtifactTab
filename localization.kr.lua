5ede39f29b06fac75102ebfe7e8784e444fbf460
Ignifazius <stefan.hobohm@gmail.com>
2016-10-27 22:48:42 +0200

    adding korean localization (thanks to Elnarfim)

---
+++ localization.kr.lua
@@ -0,0 +1,52 @@
+local _, L = ...;
+if GetLocale() == "koKR" then
+	L["Fishing"] = "낚시"
+	-- DK
+	L["Frost"] = "냉기"
+	L["Unholy"] = "부정"
+	L["Blood"] = "혈기"
+	-- Druid
+	L["Guardian"] = "수호"
+	L["Feral"] = "야성"
+	L["Restoration"] = "회복"
+	L["Balance"] = "조화"
+	-- Paladin
+	L["Holy"] = "신성"
+	L["Retribution"] = "징벌"
+	L["Protection"] = "보호"
+	-- Rogue
+	L["Outlaw"] = "무법자"
+	L["Assassination"] = "암살"
+	L["Sublety"] = "잠행"
+	-- DH
+	L["Vengeance"] = "복수"
+	L["Havoc"] = "파멸"
+	-- Warlock
+	L["Destruction"] = "파괴"
+	L["Demonology"] = "악마"
+	L["Affliction"] = "고통"
+	-- Mage
+	L["Fire"] = "화염"
+	L["Arcane"] = "비전"
+	--L["Frost"] = "냉기"
+	-- Priest
+	--L["Holy"] = "신성"
+	L["Discipline"] = "수양"
+	L["Shadow"] = "암흑"
+	-- Monk
+	L["Brewmaster"] = "양조"
+	L["Mistweaver"] = "운무"
+	L["Windwalker"] = "풍운"
+	-- Warrior
+	L["Fury"] = "분노"
+	L["Arms"] = "무기"
+	--L["Protection"] = "방어"
+	-- Shaman
+	L["Elemental"] = "정기"
+	--L["Restoration"] = "복원"
+	L["Enhancement"] = "고양"
+	-- Hunter
+	L["Beast Mastery"] = "야수"
+	L["Marksmanship"] = "사격"
+	L["Survival"] = "생존"
+end
